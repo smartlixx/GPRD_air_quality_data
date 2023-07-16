1ac5ac8e9e4d6d570da454956e777fed4c7a9320
Xianxiang Li <smartlixx@gmail.com>
2023-07-16 17:52:21 +0800

    Update according to the website change

--- README.md
+++ README.md
@@ -3,6 +3,8 @@ This tool scapes the Guangdong-Hong Kong-Macau PRD air quality monitoring system
 data in Pearl River Delta. The system uses Microsoft Silverlight to render the data, and this tool relies on the package wcf (https://github.com/ernw/python-wcfbin)
 to decode the POST requests. 
 
+***Update*** Nov 2, 2021: the website has been changed and get rid of Silverlight. 
+
 ## Dependencies
  - wcf (https://github.com/ernw/python-wcfbin
  - requests
--- air_GPRD_class.py
+++ air_GPRD_class.py
@@ -1,71 +1,63 @@
+# Modified on Nov 2, 2021 to adapt to the new server changes
+
 #-*- encoding: utf-8 -*-
-import requests, xmltodict, json # re, base64, zlib, 
+import requests, re, xmltodict, json 
 import time
-from io import BytesIO,StringIO
-from wcf.records import *
-from wcf.xml2records import XMLParser
 import PRD_stations      # All the stations in the GPRD area
 
-# https://github.com/ernw/python-wcfbin
+
 
 class air_GPRD():
     def __init__(self):
         self.stationList = json.loads(PRD_stations.PRD_stationList_json)
         self.pollutantcode = (101,102,103,104,108,109,110)
 
-    def getResponse(self, service, action, data):
-        output = StringIO()
-        output.write('<'+action+' xmlns="http://tempuri.org/">'+data+'</'+action+'>')
-        output.seek(0)
+    def getResponse(self, action, data):
+        d = '<s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/"><s:Body><'+action+' xmlns="http://tempuri.org/">'+data+'</'+action+'></s:Body></s:Envelope>' #)
 
-        r = XMLParser.parse(output)
-        req = dump_records(r)
-
-        r = requests.post(url='http://113.108.142.147:20047/ClientBin/Env-GPRD-BLL-Services-' 
-                            + service + '.svc/binary/' + action,
-            data=req,
-            headers={'Content-Type': 'application/msbin1',
+        r = requests.post(url='http://113.108.142.147:20047/GprdWcf/GprdDataService.svc',
+            data=d,
+            headers={'Content-Type': 'text/xml; charset=utf-8', 
+                'SOAPAction': 'http://tempuri.org/IService1/'+action, #'application/msbin1',
                 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.95 Safari/537.36'})
         r.encoding = "utf-8"
+
         res = r.content
-
-        buf = BytesIO(res)
-        r = Record.parse(buf)
-
-        print_records(r, fp=output)
-        output.seek(0)
-        #print(output.getvalue())
-        output = output.readlines()[1:-1]
-        output = ''.join(output)
-
-        convertedDict = xmltodict.parse(output.replace('&',''), xml_attribs=False)
+        convertedDict = xmltodict.parse(res, xml_attribs=False)['s:Envelope']['s:Body']
         return json.dumps(convertedDict)
 
     def getAllStations(self,languageId=3):
     # languageID = 1: Simplified Chinese
     #              2: Traditional Chinese
     #              3: English
-        return json.loads(self.getResponse("StationService", "GetAllStations",
-               "<languageId>"+str(languageId)+"</languageId>"))["GetAllStationsResult"]["a:StationInfoModel"]
+        return json.loads(self.getResponse("GetAllStations",
+            "<languageId>"+str(languageId)+"</languageId>"))['GetAllStationsResponse']["GetAllStationsResult"]["a:StationInfoModel"]
 
     def getAllSystemConfigs(self, languageId=3):
-        return json.loads(self.getResponse("SystemService", "GetAllSystemConfigs", 
-               "<languageID>"+str(languageId)+"</languageID>"))["GetAllSystemConfigsResult"]["a:ConfigModel"]
+        return json.loads(self.getResponse("GetAllSystemConfigs", 
+               "<languageID>"+str(languageId)+"</languageID>"))['GetAllSystemConfigsResponse']["GetAllSystemConfigsResult"]["a:ConfigModel"]
 
     def getAllStationsInfo(self):
         return self.stationList
         #return json.loads(PRD_stations.PRD_stationList_json)
 
     # TODO: make this return the stations belonging to each region (Guangdong, Hong Kong and Macau)
-    #def getProvinceAllStationInfo(self, AreaId):
+    #def getProvinceAllStationInfo(self, pid):
+    #    pjson = json.loads(station_data.stationList_json)
+    #    ret = []
+    #    for c in pjson[str(pid)]:
+    #        for s in pjson[str(pid)][c]:
+    #            ret.append(s)
+    #
+    #    return ret
 
     def getPublishTime(self):
-        return json.loads(self.getResponse("SystemService", "GetPublishTime", ""))['GetPublishTimeResult']
+        return json.loads(self.getResponse("GetPublishTime", ""))['GetPublishTimeResponse']['GetPublishTimeResult']
 
     def getSinglePollution(self,pollutantcode,stationcode):
-        return json.loads(self.getResponse("PollutationService", "GetSinglePollution", 
+        return json.loads(self.getResponse("GetSinglePollution", 
                "<pollutantcode>"+str(pollutantcode)+"</pollutantcode>" +
-               "<stationcode>"+str(stationcode)+"</stationcode>"))["GetSinglePollutionResult"]
+               "<stationcode>"+str(stationcode)+"</stationcode>"))['GetSinglePollutionResponse']["GetSinglePollutionResult"]
     
     def getAllPollutants(self):
         for key,_ in self.stationList.items():
@@ -75,10 +67,11 @@ class air_GPRD():
                     data = self.getSinglePollution(pl,key)
                     f.writelines(data['a:PollutantString']+","+data['a:LastHourValue']+"\n")
 
-    def getSingleAirIndex(self,stationcode,languageId=3):
-        return json.loads(self.getResponse("InfomationService","GetSingleAirIndex",
+    def getSingleAirIndex(self,languageId,stationcode):
+        # This function has no returned value
+        return json.loads(self.getResponse("GetSingleAirIndex",
                "<languageId>"+str(languageId)+"</languageId>" +
-               "<stationcode>"+str(stationcode)+"</stationcode>"))["GetSingleAirIndexResult"]
+               "<stationcode>"+str(stationcode)+"</stationcode>"))['GetSingleAirIndexResponse']["GetSingleAirIndexResult"]
 
 if __name__ == '__main__':
     air = air_GPRD()
